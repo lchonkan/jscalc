f0addf44807b72ad624a49d1edc5587dd2d1ed2e
Leonardo Chonkan <lchonkan@forgebim.com>
2020-05-04 00:00:27 -0600

    working demo

--- script.js
+++ script.js
@@ -1,11 +1,94 @@
-var pantalla = document.getElementById('pantalla');
-const button = document.querySelector('keypad');
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+const screen = document.getElementById('pantalla');
 
+document.querySelector(".keypad").addEventListener("click", function (event) {
+    buttonClick(event.target.innerText);
 
-const updateDisplay = function (value) {
-    this.pantalla.innerText = value;
+})
+
+function buttonClick(value) {
+    console.log(value);
+    if (isNaN(parseInt(value))) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
 }
 
-updateDisplay("1234567890");
+function handleNumber(value) {
+    if (buffer === "0") {
+        buffer = value;
+    } else {
+        buffer += value;
+    }
 
-//TODO: Event Listeners
+
+    rerender();
+}
+
+function handleSymbol(value) {
+    switch (value) {
+        case 'C':
+            buffer = "0";
+            runningTotal = 0;
+
+            break;
+
+        case '=':
+            if (previousOperator === null) {
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
+            buffer = "" + runningTotal;
+            runningTotal = 0;
+            break;
+
+        case "←":
+            if (buffer.length === 1) {
+                buffer = "0";
+            } else {
+                buffer = buffer.split(1);
+            }
+            break;
+
+        default:
+            handleMath(value);
+            break;
+    }
+
+    rerender();
+}
+
+
+function handleMath(value) {
+    const intBuffer = parseInt(buffer);
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = value;
+
+    buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === "-") {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === "x") {
+        runningTotal *= intBuffer;
+    } else {
+        runningTotal /= intBuffer;
+    }
+
+}
+
+function rerender() {
+    screen.innerText = buffer;
+}
